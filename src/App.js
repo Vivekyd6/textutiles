1a1c5db73081180227db933f768f19aa70331240
Vivek <86094260+Vivekyd6@users.noreply.github.com>
2021-12-16 14:58:46 +0530

    Completed Dark Mode and Many other functionalities

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import React, { useState } from 'react';
+import Alert from './components/Alert';
+//import About from './components/About';
 
 function App() {
+  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled", "success");
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
+      {/* <Navbar/> */}
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <div>
+        <Alert alert={alert}/>
+      </div>
+      <div className="container my-3">
+        <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />
+      </div>
+     
+    </>
   );
 }
 
